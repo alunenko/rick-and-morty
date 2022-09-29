f9ed20a0020a3d5afb75d2918e8ea0fcd56eef38
Anton Lunenko <antoni.lunenko@gmail.com>
2022-09-29 12:27:34 +0200

    3: create route for all characters page and inject it to app.component view

--- workspace/projects/rick-and-morty/src/app/app-routing.module.ts
+++ workspace/projects/rick-and-morty/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CharactersComponent} from "./components/pages/characters/characters.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'all-characters',
+    pathMatch: 'full'
+  },
+  {
+    path: 'all-characters',
+    component: CharactersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
